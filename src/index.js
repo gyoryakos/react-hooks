c12c05524c23deaf9a2e56be870dfa230ef3ce12
gyoryakos <gyoryakos@gmail.com>
2022-11-08 08:28:07 +0100

    add useState, useReducer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
 // useState
-import App from './project_useState/App';
+//import App from './project_useState/App';
 
+// useReducer
+//import App from './project_useReducer/App';
+
+// useEffect
+import App from './project_useEffect/App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
--- src/project_useState/App.js
+++ src/project_useState/App.js
@@ -2,13 +2,27 @@ import React, {useState} from 'react';
 
 export default function App() {
   const [counter, setCounter] = useState(0);
+  const [inputValue, setInputValue] = useState("Ákos");
+
   function increment() {
     setCounter(count => ++count);
   }
+
+  let handleInput = event => {
+    const newValue = event.target.value;
+    setInputValue(newValue);
+  }
+
   return (
-    <div>
-      {counter}
-      <button onClick={increment }>Increment</button>
-    </div>
+    <>
+      <div>
+        {counter}
+        <button onClick={increment }>Increment</button>
+      </div>
+      <div>
+        <input placeholder='enter something...' onChange={handleInput}/>
+        {inputValue}
+      </div>
+    </>
   );
 }
